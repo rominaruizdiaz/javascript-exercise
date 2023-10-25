1bb1a351241e80ad5639b0bab8f628cfb9513c6a
Rominaruizdiaz <ruizdiazsanchezromina@gmail.com>
2023-10-25 18:15:51 +0200

    exercise 2 done

---
+++ src/ex-2/app.js
@@ -0,0 +1,29 @@
+/*
+Escribe un programa que escriba en la pantalla “La suma de 3 + 5 es <resultado>”.
+* Conectar archivo javascript y css al html.
+* Imprimir ”La suma de 3 + 5 es <resultado>” en nuestra pantalla.
+* * Crear un contenedor para el mensaje en html con un id.
+* * Conseguir el resultado de la suma.
+* * * Declarar variables de los numeros que queremos sumar.
+* * * Declarar el resultado como la suma de las variables anteriores.
+* * Mostrar el resultado como mensaje en la pantalla.
+* * * Declarar variable del mensaje que queremos enviar.
+* * * Declarar variable por como queremos llamarlo. (por id).
+* * * Declarar una función para imprimir el mensaje.
+* * * Dentro del objeto con el que vamos a llamar, escribir la variable que queremos enviar.
+* * * llamar a la función para que funcione.
+*/
+
+const numberOne = 3;
+const numberTwo = 5;
+const result = numberOne + numberTwo;
+
+const addMsg = "La suma de " + numberOne + " y " + numberTwo + " es " + result;
+const addSection = document.getElementById("addMsg");
+
+function addingNumbers() {
+    addSection.innerHTML = `
+        <h1>${addMsg}</h1>
+    `
+}
+addingNumbers()
