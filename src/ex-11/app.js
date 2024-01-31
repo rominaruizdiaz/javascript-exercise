45316f20328c29976a5c796837847dbae4bcb7fc
Rominaruizdiaz <ruizdiazsanchezromina@gmail.com>
2024-01-31 22:42:32 +0100

    exercise 11 done

---
+++ src/ex-11/app.js
@@ -0,0 +1,28 @@
+/*Escribe un programa que pida un número y nos diga si es divisible por 2, 3, 5 o 7 (sólo hay que comprobar si lo es por uno de los cuatro)
+ 
+Conectar archivo javascript y css al html.
+Crear el formulario.
+almacenar el numero en una variable.
+buscar que numeros son divisibles por el numero y almacenarlos.
+mostrar el resultado en el html.
+*/
+
+function findDivisors() {
+  let number = parseInt(document.getElementById("number").value);
+
+  let divisors = [2, 3, 5, 7];
+  let yesDivisors = [];
+
+  
+  for (let divisor of divisors) {
+    if (number % divisor === 0) {
+      yesDivisors.push(divisor)
+    }
+  }
+
+  if (yesDivisors.length > 0) {
+    result.innerHTML += `<p>${number} es divisible por: ${yesDivisors.join(', ')}.</p>`;
+  } else {
+    result.innerHTML += `<p>${number} no es divisible por ningún número en la lista.</p>`;
+  }
+}
